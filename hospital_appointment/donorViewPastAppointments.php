3c42b09307e3345fbfa5a024d08bc8807580d4fe
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2022-10-19 21:40:55 +0300

    Update donorViewPastAppointments.php

--- hospital_appointment/donorViewPastAppointments.php
+++ hospital_appointment/donorViewPastAppointments.php
@@ -33,7 +33,7 @@ $result = $connection->query($sql);
         <header>     
         <nav>
             <img id="logo" src="../images/Logo.png" width="80"height="80"> 
-            <a href="../homepage.php" style="margin-left:15px;">Home</a> 
+            <a href="../index.php" style="margin-left:15px;">Home</a> 
             <a href="../donation_requirements.php" style="margin-left:15px;">Eligibility</a> 
             <a href="../RegisteredHospitals.php" style="margin-left:15px;">Partnered Hospitals</a> 
 
